e26b7be614b1b69e468bf49bbc74ea6dda114f6e
dev-komenzar <takuya.kometan@gmail.com>
2023-03-20 15:31:36 +0900

    First commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+	plugins: [require.resolve('prettier-plugin-astro')],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+	semi: false,
+	useTabs: true,
+	tabWidth: 2,
+	printWidth: 80,
+	singleQuote: true,
+	trailingComma: 'all',
+	jsxSingleQuote: true,
+	bracketSpacing: true,
+}
