78566f8e5d048d433e0eba6b520455d62d13d2e5
dev-komenzar <takuya.kometan@gmail.com>
2023-12-16 11:25:30 +0900

    Feat: Install TailwindCSS

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,10 +1,24 @@
 module.exports = {
-	plugins: ['stylelint-scss'],
+	plugins: ['stylelint-scss', 'stylelint-prettier'],
 	extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
 	rules: {
+		'at-rule-no-unknown': null,
+		'scss/at-rule-no-unknown': [
+			true,
+			{
+				ignoreAtRules: [
+					'tailwind',
+					'apply',
+					'variants',
+					'responsive',
+					'screen',
+				],
+			},
+		],
 		'font-family-no-missing-generic-family-keyword': true,
 		'declaration-block-no-shorthand-property-overrides': true,
 		'selector-pseudo-element-colon-notation': 'double',
+		'prettier/prettier': true,
 	},
 	customSyntax: 'postcss-html',
 	ignoreFiles: ['**/*.json'],
